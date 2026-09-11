31f986b099eb7e278a04c445dd6d534fd45737db
DakiBebek <92137245+DakiBebek@users.noreply.github.com>
2026-09-11 16:36:22 +0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  // Otomatis arahkan siapa pun yang buka root website ke halaman login
+  // Otomatis arahkan ke halaman login
   redirect('/login')
 }
