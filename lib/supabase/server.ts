637f4f9ed6e5431be52254d42d3f3f5b77feb626
DakiBebek <92137245+DakiBebek@users.noreply.github.com>
2026-09-11 16:00:16 +0700

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,19 +1,20 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export const createClient = () => {
+export const createClient = async () => {
+  const cookieStore = await cookies()
+  
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: async () => {
-          const cookieStore = await cookies()
+        getAll() {
           return cookieStore.getAll()
         },
-        setAll: async (cookiesToSet) => {
-          const cookieStore = await cookies()
+        setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) => {
+            // PENTING: Masukkan 'options' di sini juga
             cookieStore.set(name, value, options)
           })
         },
